d39206d2010f38fb6166edde4e3520debf94f6e2
Adao Pedro <adao.pedro16@gmail.com>
2023-12-11 18:53:38 +0100

    first commit

---
+++ form.php
@@ -0,0 +1,12 @@
+<?php
+// Para testes
+    echo "<pre>";
+    print_r($_POST);
+    echo "</pre>";
+
+    echo "<hr>";
+    
+    echo "<pre>";
+    print_r($_SERVER["QUERY_STRING"]);
+    echo "</pre>";
+
